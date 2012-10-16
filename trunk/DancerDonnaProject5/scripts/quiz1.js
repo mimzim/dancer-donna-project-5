45f673edd8da70d0b59d857ba3228e7a1e5d918d
ddancer@satx.rr.com <ddancer@satx.rr.com@32268b76-a05f-bde9-10fe-0ca8aeb1a711>
2012-10-16 19:27:46 +0000

    Added questions to quiz, updated quiz js file.

--- trunk/DancerDonnaProject5/scripts/quiz1.js
+++ trunk/DancerDonnaProject5/scripts/quiz1.js
@@ -1,18 +1,49 @@
 // This function processes the quiz data
-function submitQuiz1()
+function submitQuiz()
 {
+	var numberCorrect = 0;
 	var q1 = document.forms.quiz1.question1;
+	
+	if( q1[ 1 ].checked == true )
+		{
+		numberCorrect = numberCorrect + 1;
+		}
+	
 	var q2 = document.forms.quiz1.question2;
 	
-	if( q1[ 0 ].checked == true && q2[ 2 ].checked == true )
-	{
-		alert( "You passed!" );
-	}
-	else
-	{
-		alert( "you failed" );
-	}
+	if ( q2[ 0 ].checked == true )
+		{
+		numberCorrect = numberCorrect + 1;
+		}
 	
-	// call the function to report scores
-	parent.reportScores();
+	var q3 = document.forms.quiz1.question3;
+	
+	if ( q3[ 2 ].checked == true )
+		{
+		numberCorrect = numberCorrect + 1;
+		}
+	
+	var q4 = document.forms.quiz1.question4;
+	
+	if ( q4[ 2 ].checked == true )
+		{
+		numberCorrect = numberCorrect + 1;
+		}
+	
+	var q5 = document.forms.quiz1.question5;
+	
+	if ( q5[ 0 ].checked == true )
+		{
+		numberCorrect = numberCorrect + 1;
+		}
+	
+	var q6 = document.forms.quiz1.question6;
+	
+	if ( q6[ 1 ].checked == true )
+		{
+		numberCorrect = numberCorrect + 1;
+		}
+	
+var score= numberCorrect /6;
+alert( numberCorrect /6 );
 }
