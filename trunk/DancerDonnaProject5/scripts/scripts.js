0a7462c08746b94a9c08685cf4ef60d66f19a96b
ddancer@satx.rr.com <ddancer@satx.rr.com@32268b76-a05f-bde9-10fe-0ca8aeb1a711>
2012-10-16 14:46:30 +0000

    Renamed files to match Project 4, since that was successful for me last time.  I did not rename Topic 1, which is the home page, since we are not allowed to modify the index.html file.  Fixed JavaScript display quiz function that wasn't working.  Removed the SCORM info from JavaScript page, since it is not part of this project and made it cluttered with no purpose.  Renamed my function to startCourse, since that worked for me last time and is logical.

--- trunk/DancerDonnaProject5/scripts/scripts.js
+++ trunk/DancerDonnaProject5/scripts/scripts.js
@@ -1,12 +1,7 @@
-// define a SCORM object to interface with LMS
-var oScorm = pipwerks.SCORM;  
-
-// This function starts the course.  This function should be called first to ensure that
-// all session variables are created
-function loadOLM()
+function startCourse()
 {
 	// check to see if cookies is enabled, otherwise, this won't work
-	if( typeof sessionStorage == "undefined" || sessionStorage.disabled )
+	if( typeof sessionStorage === "undefined" || sessionStorage.disabled )
 	{
 		alert( "You must enable cookies for this course to work properly." );
 	}
@@ -20,78 +15,15 @@ function loadOLM()
         	// clear the session storage
             sessionStorage.clear();
             // set the "key" by giving it a value.  all values are strings!
-            // course_started indicated that this OLM has started
     		sessionStorage.setItem( "course_started", "1" );
-    			
-			// set the values you want to persist throughout the website here
+    		sessionStorage.setItem( "home", "0" );
     		sessionStorage.setItem( "botswana", "0" );
     		sessionStorage.setItem( "finland", "0" );
     		sessionStorage.setItem( "palau", "0" );
     		sessionStorage.setItem( "uae", "0" );
-        
+    		
         }
 	}
-	
-	// initializeSCORM after the OLM environment has been set up
-	initializeSCORM();
-}
-
-// This function initializes the course by connecting the OLM to the LMS 
-// The SCORM connection is done once at the beginning of the course
-// Another function, reportScores(), will connect again when the 
-// user is done with the online learning module
-function initializeSCORM()
-{
-	var lmsConnected = oScorm.init();
-	
-	// we only want to run initializeSCORM once, so use course_started
-	// to keep track of how many times we have initialized 
-	var getStarted = sessionStorage.getItem( "course_started" );
-	
-	// only do the following if we are connected to the LMS and
-	// if course_started has a value of 1
-	if( lmsConnected && getStarted == "1" )
-	{
-        // always set the status to incomplete
-		oScorm.set( "cmi.success_status", "unknown" );
-		oScorm.set( "cmi.lesson_status", "incomplete" );
-		oScorm.set( "cmi.completion_status", "incomplete" );
-		
-		// change course_started to another value so that this initializeSCORM
-		// does not run the initialization code again; otherwise, this OLM
-		// will be incomplete if the user goes back to the first page even after
-		// passing the exam 
-		sessionStorage.setItem( "course_started", "2" );
-    		
-		// retrieve the LMS values here and integrate them into your course
-		
-	}
-}
-
-// This function reports the score from the assessment to the LMS
-// This should only be called when the user submits the answers to the quiz
-function reportScores()
-{	
-	// these are some test values
-	oScorm.set("cmi.score.raw", 83 );
-	oScorm.set("cmi.score.min", 0 );
-	oScorm.set("cmi.score.max", 100 );
-	oScorm.set("cmi.score.scaled", .83 );
-	
-	// Set the values and status here
-	// for now, just set it to passed and completed
-	oScorm.set( "cmi.success_status", "passed" );
-	oScorm.set( "cmi.completion_status", "completed" );
-	oScorm.set( "cmi.lesson_status", "passed" );
-
-	oScorm.save();
-}
-
-// This function is called when the window is closed.  It saves and quits the course.
-function finishOLM()
-{
-	oScorm.save();
-	oScorm.quit();
 }
 
 function visitBotswana()
